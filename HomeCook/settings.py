fba9611ecb190b73c8616d3ee61c76435f49dcab
Isala Piyarisi <code@isala.me>
2019-05-25 22:50:08 +0530

    More ignore

--- HomeCook/settings.py
+++ HomeCook/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-AWS_DEFAULT_ACL = os.getenv('S3_ACCESS_KEY_ID')
+AWS_DEFAULT_ACL = os.getenv('AWS_DEFAULT_ACL')
 AWS_ACCESS_KEY_ID = os.getenv('S3_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('S3_STORAGE_BUCKET_NAME')
