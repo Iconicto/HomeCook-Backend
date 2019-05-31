5fa7369555390d0decd867c0a7e66cf057fce0eb
Isala Piyarisi <code@isala.me>
2019-05-31 22:39:03 +0530

    mergederp

--- Recipe/urls.py
+++ Recipe/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.vision_api, name='VisionAPI'),
+    path('api/vision', views.vision_api, name='VisionAPI'),
 ]
